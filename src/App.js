3e1fc03fa755f7438a5ce90305cfb83e7ffe77ea
sharmaji-2407 <prakharksharma@gmail.com>
2021-10-27 17:30:02 +0530

    functionality added with basic css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import {Card} from 'react-bootstrap';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [tog_dark, settog_dark] = useState(false);
+  const [theme, settheme] = useState("light")
+  const handleclick = () => {
+    settog_dark(!tog_dark)
+    if(!tog_dark)
+    settheme("dark");
+    
+    else
+    settheme("light");
+  };
+  const theme_card = theme+"_card";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={theme} id="main">
+      <h1>React app for theme toggling</h1>
+      
+      <div className="toggler">
+        <label className="switch">
+          <input type="checkbox" checked={tog_dark} onChange={handleclick} />
+          <span className="slider  round" />
+        </label>
+      </div>
+
+      <div  className={theme_card+" card"} style={{width: 400}}>
+        <img className="card-img-top" src="img_avatar1.png" alt="Card image" style={{width: '100%'}} />
+        <div className="card-body">
+          <h4 className="card-title">{theme}</h4>
+          <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam </p>
+          
+        </div>
+      </div>
+
+      
+
+
+      
+
+
     </div>
   );
 }
