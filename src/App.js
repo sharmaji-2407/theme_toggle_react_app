1169d81dbbe883fe54364b78062000a37965b630
sharmaji-2407 <prakharksharma@gmail.com>
2021-10-28 18:33:17 +0530

    card position fixed

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import './App.css';
 const App = () => {
 
   const [tog_dark, settog_dark] = useState(false);
-  const [theme, settheme] = useState("light")
+  const [theme, settheme] = useState("light");
+  const [theme_rev, settheme_rev] = useState("dark");
   const handleclick = () => {
     settog_dark(!tog_dark)
     if(!tog_dark)
+    {
     settheme("dark");
-    
-    else
-    settheme("light");
+    settheme_rev("light")
+    }
+    else{
+      settheme("light");
+      settheme_rev("dark");
+    }
+
   };
-  const theme_card = theme+"_card";
+  //const theme_card = theme+"_card";
 
   return (
     <div className={theme} id="main">
@@ -28,8 +34,8 @@ const App = () => {
         </label>
       </div>
 
-      <div  className={theme_card+" card"} style={{width: 400}}>
-        <img className="card-img-top" src="img_avatar1.png" alt="Card image" style={{width: '100%'}} />
+      <div  className={theme_rev+" card mx-auto"} style={{width: 400, top: 20 }}>
+        {/* <img className="card-img-top" src="img_avatar1.png" alt="Card image" style={{width: '100%'}} /> */}
         <div className="card-body">
           <h4 className="card-title">{theme}</h4>
           <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam </p>
